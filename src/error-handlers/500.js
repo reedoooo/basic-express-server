d8328f987426fd8153803c7c970cb57dd2d8ba50
Reed Vogt <readvogt@gmail.com>
2023-05-16 13:10:50 -0700

    added the file

--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Error500 Middleware
@@ -7,7 +7,7 @@
 const error500 = (request, response, next) => {
   // If 'name' query is missing, respond with error
   if (!request.query.name) {
-    console.log('error500.js hit');
+    console.log("error500.js hit");
 
     let err = {
       message: "Missing value for 'name' query",
@@ -15,7 +15,7 @@ const error500 = (request, response, next) => {
 
     response.status(500).send(err);
   } else {
-    next(); // If 'name' query is present, proceed to next middleware   
+    next(); // If 'name' query is present, proceed to next middleware
   }
 };
 
--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -3,8 +3,7 @@
 const validator = (request, response, next) => {
   if (request.query.name) {
     next();
-  }
-  else {
+  } else {
     throw new Error("Missing 'name' query");
   }
 };
